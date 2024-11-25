a4ae951bf0575713559c47e99b532f6d9763d46c
urbanpotocnik <potocnik.urban123@gmail.com>
2024-11-25 14:47:18 +0100

    Changed some things

--- vaja6/script.py
+++ vaja6/script.py
@@ -5,27 +5,23 @@ parent_dir = "/home/urban/Faks/Obdelava slik in videa/Vaje"
 sys.path.append(parent_dir)
 from OSV_lib import loadImage, displayImage
 
-def getRadialValues(iXY, iCP):
-    K = iCP.shape[0]
+# Naloga 1:
+if __name__ == "__main__":
+    imSize = [256,512]
+    pxDim = [2,1]
 
-    # instanciranje izhodnih radialnih uteži
-    oValue = np.zeros(K)
+    gX = np.arange(imSize[0])*pxDim[0]
+    gY = np.arange(imSize[1])*pxDim[1]
+    I = loadImage("vaja6/data/lena-256x512-08bit.raw", imSize, np.uint8)
+    displayImage(I, "Originalna slika", gX,gY)
 
-    x_i, y_i = iXY
-    for k in range(K):
-        x_k, y_k = iCP[k]
-
-        # razdalja vhodne tocke do k-te kontrolne točke
-        r = np.sqrt((x_i - x_k) ** 2 + (y_i -y_k) ** 2)
-
-        # apliciranje radialne funkcije na r
-        if r > 0:
-            oValue[k] = -(r**2) * np.log(r)
-
-    return oValue
-
+# Naloga 2:
 def getParameters(iType, scale = None, trans = None, rot = None, shear = None, orig_pts = None, mapped_pts = None):
-    # default values
+    # za afino preslikavo: scale je vektor skaliranja oz. povecave, trans je vektor translacije oz. premika, rot = φ je kot rotacije
+    # oz. vrtenja v kotnih stopinjah in shear je vektor striga oz. zatega 
+
+    # za radialno preslikavo: orig_pts je matrika kontrolnih tock in mapped_pts je matrika preslikanih kontrolnih tock
+
     oP = {}
 
     if iType == "affine":
@@ -83,8 +79,31 @@ def getParameters(iType, scale = None, trans = None, rot = None, shear = None, o
         
     return oP
 
+if __name__ == "__main__":
+    T = getParameters("affine", rot=30)
+    print(T)
 
-def transformImage(iType, iImage, iDim, iP, iBgr=0, iInterp=0):
+# Naloga 3:
+def getRadialValues(iXY, iCP):
+    K = iCP.shape[0]
+
+    # instanciranje izhodnih radialnih uteži
+    oValue = np.zeros(K)
+
+    x_i, y_i = iXY
+    for k in range(K):
+        x_k, y_k = iCP[k]
+
+        # razdalja vhodne tocke do k-te kontrolne točke
+        r = np.sqrt((x_i - x_k) ** 2 + (y_i -y_k) ** 2)
+
+        # apliciranje radialne funkcije na r
+        if r > 0:
+            oValue[k] = -(r**2) * np.log(r)
+
+    return oValue
+
+def transformImage(iType, iImage, iDim, iP, iInterp, iBgr=0):
     Y, X = iImage.shape
     dx, dy = iDim
 
@@ -109,28 +128,27 @@ def transformImage(iType, iImage, iDim, iP, iBgr=0, iInterp=0):
                     oImage[y, x] = iImage[y_hat,x_hat]    
     return oImage
 
+
 if __name__ == "__main__":
     imSize = [256,512]
     pxDim = [2,1]
 
     gX = np.arange(imSize[0])*pxDim[0]
     gY = np.arange(imSize[1])*pxDim[1]
-    I = loadImage("vaja6/data/lena-256x512-08bit.raw", imSize, np.uint8)
-    displayImage(I, "Originalna slika", gX,gY)
 
     T = getParameters("affine", rot=30)
     print(T)
 
     brg = 63
 
-    tImage = transformImage("affine", I, pxDim, np.linalg.inv(T), iBgr=brg)
+    tImage = transformImage("affine", I, pxDim, np.linalg.inv(T), iInterp = 0, iBgr=brg)
     displayImage(tImage, "Affina preslikava", gX,gY)
 
     xy = np.array([[0,0],[511,0],[0,511],[511,511]])
     uv = np.array([[0,0],[511,0],[0,511],[255,255]])
     P = getParameters("radial", orig_pts=xy, mapped_pts=uv)
     print(P)
-    rImage = transformImage("radial", I, pxDim, P, iBgr=brg)
+    rImage = transformImage("radial", I, pxDim, P, iInterp = 0, iBgr=brg)
     displayImage(rImage, "Radialna preslikava", gX,gY)
 
-    # to do: uredi celo kodo, 2 maina, osv lib, jupyter notebook
+# to do: uredi celo kodo, 2 maina, osv lib, jupyter notebook
